62ed3dcd27a47a4c72ad3431c5fd0979b30abc7a
Adrien Quillet <adrien.quillet@icloud.com>
2024-04-27 19:13:13 +0200

    Add some documentation

--- addons/godot-jam-toolkit/audio/haudio_manager.gd
+++ addons/godot-jam-toolkit/audio/haudio_manager.gd
@@ -1,6 +1,13 @@
 extends Node
 # Autoload
 
+##
+## Audio manager for Godot.
+## [br][br]
+## This autoload allows to easily play sounds, musics, sfx effects in your game. Each audio can
+## be played on a bus, and can also be played on a specific channel for a more precise audio management.
+##
+
 #------------------------------------------
 # Constants
 #------------------------------------------
@@ -29,49 +36,90 @@ var _buses_by_name:Dictionary = {}
 #------------------------------------------
 
 func _ready() -> void:
+    # Buses can be configured via editor. There is the Master bus by default,
+    # plus other user-defined buses
     _initialize_default_buses()
 
 #------------------------------------------
 # Public functions
 #------------------------------------------
 
+## TODO channel limit
+
+## Get the list of available output devices. See [method set_output_device] to modify the
+## current output device.
 func get_output_devices() -> PackedStringArray:
     return AudioServer.get_output_device_list()
 
+## Modify the audio output device for the application. See [method get_output_devices] to get
+## available device list.
 func set_output_device(device_name:String) -> void:
     AudioServer.output_device = device_name
 
 
-
+## Modify the volume of the given bus. Volume is expressed in percent (from [code]0[/code] to [code]1[/code])
+## and automatically transformed into decibel.
+## [br]
+## If [code]channel_index[/code] is [code]-1[/code], all bus channels
+## are affected, else only the specified channel is affected.
 func set_volume(bus_name:String, volume_percent:float, channel_index:int = -1) -> void:
     _get_bus_by_name(bus_name).set_volume(volume_percent, channel_index)
 
+## Returns the volume of the given bus, in percent (from [code]0[/code] to [code]1[/code]).
+## Use [method @GlobalScope.linear_to_db] to transform this value into decibels.
+## [br]
+## If [code]channel_index[/code] is [code]-1[/code], returns the bus volume, else returns
+## the volume of the specified channel.
 func get_volume(bus_name:String, channel_index:int = -1) -> float:
     return _get_bus_by_name(bus_name).get_volume(channel_index)
 
+## Returns [code]true[/code] if the given bus is mute, [code]false[/code] otherwise.
 func is_mute(bus_name:String) -> bool:
     return _get_bus_by_name(bus_name).is_mute()
 
+## Mute the given bus.
 func mute(bus_name:String) -> void:
     _get_bus_by_name(bus_name).mute()
 
+## Unmute the given bus. Has no effect is the bus is not muted. See also  [is_mute].
 func unmute(bus_name:String) -> void:
     _get_bus_by_name(bus_name).unmute()
 
+## Returns the global playback speed scale.
+func get_playback_speed_scale() -> float:
+    return AudioServer.playback_speed_scale
 
-
+## Modify the global playback speed scale, i.e. the rate at which audio is played.
 func set_playback_speed_scale(scale:float) -> void:
     AudioServer.playback_speed_scale = max(0.001, scale)
 
+
+## If [code]channel_index[/code] is [code]-1[/code], returns [code]true[/code] if at least one channel
+## of the specified bus is playing, [code]false[/code] otherwise.
+## [br]
+## Otherwise, returns [code]true[/code] if the given channel is playing audio, [code]false[/code] otherwise.
 func is_playing(bus_name:String, channel_index:int = -1) -> bool:
     return _get_bus_by_name(bus_name).is_playing(channel_index)
 
+## Play the given [AudioStream] in the specified bus.
+## [br][br]
+## If [code]fade_in[/code] is greater than 0, a fade in effect is applied for the given time.
+## [br][br]
+## If [code]channel_index[/code] is specified, the audio stream is played on the specific channel, otherwise it is
+## played on the first available channel of this bus. If no channel is available, a new channel is created if
+## channel limit is not reached. Playing on an active channel make the previous audio stream stop immediately,
+## without fade out effect.
 func play_audio(bus_name:String, audio:AudioStream, fade_in:float = 0.0, channel_index:int = -1) -> void:
     _get_bus_by_name(bus_name).play(audio, fade_in, channel_index)
 
+## Stop audio streams on the secified bus.
+## [br][br]
+## If [code]fade_out[/code] is greater than 0, a fade out effect is applied for the given time.
+## [br][br]
+## If [code]channel_index[/code] is specified, only this channel is stopped. Otherwise all channels of
+## the bus are stopped.
 func stop_audio(bus_name:String, fade_out:float = 0.0, channel_index:int = -1) -> void:
     _get_bus_by_name(bus_name).stop(fade_out, channel_index)
-
 
 
 #------------------------------------------
